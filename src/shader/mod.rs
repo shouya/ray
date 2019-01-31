bcb5e75f8669d0c20a90542f678023029a86a7a7
Shou Ya <shouya@users.noreply.github.com>
2019-02-01 03:10:12 +0800

    transparency

--- src/shader/mod.rs
+++ src/shader/mod.rs
@@ -2,25 +2,29 @@ use common::{Color, Hit, Ray};
 use object::Object;
 use scene::Scene;
 
+use std::rc::Rc;
+
 pub mod preset;
 
 pub mod diffusion;
-pub mod reflection;
-pub mod refraction;
-pub mod phong;
 pub mod glossy;
 pub mod mix;
-pub mod sum;
+pub mod phong;
 pub mod plain;
+pub mod reflection;
+pub mod refraction;
+pub mod sum;
+pub mod transparency;
 
 pub use self::diffusion::Diffusion;
-pub use self::reflection::Reflection;
-pub use self::refraction::Refraction;
-pub use self::phong::Phong;
 pub use self::glossy::Glossy;
 pub use self::mix::Mix;
-pub use self::sum::Sum;
+pub use self::phong::Phong;
 pub use self::plain::Plain;
+pub use self::reflection::Reflection;
+pub use self::refraction::Refraction;
+pub use self::sum::Sum;
+pub use self::transparency::{fresnel, Transparency};
 
 pub struct Incidence<'r, 'h, 'o> {
   pub ray: &'r Ray,
@@ -28,9 +32,10 @@ pub struct Incidence<'r, 'h, 'o> {
   pub obj: &'o dyn Object,
 }
 
+#[derive(Clone)]
 pub enum DynValue<T> {
   Const(T),
-  Dyn(fn(s: &Scene, i: &Incidence) -> T),
+  Dyn(Rc<Fn(&Scene, &Incidence) -> T>),
 }
 
 pub trait Shader {
@@ -56,6 +61,16 @@ where
       DynValue::Const(value) => value.clone(),
       DynValue::Dyn(f) => f(s, i),
     }
+  }
+}
+
+impl<T> DynValue<T> {
+  pub fn from_fn<F>(f: F) -> Self
+  where
+    F: Fn(&Scene, &Incidence) -> T,
+    F: 'static,
+  {
+    DynValue::Dyn(Rc::new(f))
   }
 }
 
--- src/shader/transparency.rs
+++ src/shader/transparency.rs
@@ -1,22 +1,61 @@
-mod fresnel;
+use common::{Color, Hit, Ray};
+use scene::Scene;
+use shader::{DynValue, Incidence, Mix, Reflection, Refraction, Shader};
 
-use shader::{Reflection, Refraction, Mix};
-use fresnel::fresnel;
+// return reflection ratio
+fn fresnel_internal(ray: &Ray, hit: &Hit, ior: f32) -> f32 {
+  use std::mem;
 
+  let cosi = ray.dir.dot(hit.norm);
+  let mut etai = ior;
+  let mut etat = 1.0;
+  if cosi < 0.0 {
+    mem::swap(&mut etai, &mut etat);
+  }
+  let eta = etai / etat;
+
+  let sint2 = eta * eta * (1.0 - cosi * cosi).max(0.0);
+  if sint2 >= 1.0 {
+    return 1.0;
+  }
+
+  let cost = (1.0 - sint2).max(0.0).sqrt();
+  let cosi = cosi.abs();
+  let rs = (etat * cosi - etai * cost) / (etat * cosi + etai * cost);
+  let rp = (etai * cosi - etat * cost) / (etai * cosi + etat * cost);
+  (rs * rs + rp * rp) / 2.0
+}
+
+pub fn fresnel(ior: &DynValue<f32>) -> DynValue<f32> {
+  let ior = ior.clone();
+  let f = move |s: &Scene, i: &Incidence| {
+    let ior = ior.get(s, i);
+    fresnel_internal(i.ray, i.hit, ior)
+  };
+  DynValue::from_fn(f)
+}
 
 pub struct Transparency {
-  refl: Reflection,
-  refr: Refraction
+  mix: Mix,
+  // refl: Reflection,
+  // refr: Refraction,
 }
 
 impl Shader for Transparency {
   fn render_depth(&self, s: &Scene, i: &Incidence, d: usize) -> Option<Color> {
-    let frac = fresnel(self.refr.ior);
-    let mix = Mix::new(
-      Box::new(self.refl),
-      Box::new(self.refr),
-      frac
-    );
-    mix.render_depth(s, i, d)
+    self.mix.render_depth(s, i, d)
+  }
+}
+
+impl Transparency {
+  fn new(reflectivity: DynValue<f32>, ior: DynValue<f32>) -> Self {
+    let refl = Reflection { reflectivity };
+    let refr = {
+      let ior2 = ior.clone();
+      Refraction { ior: ior2 }
+    };
+    let frac = fresnel(&ior);
+    let mix = Mix::new(Box::new(refl), Box::new(refr), frac);
+    Self { mix }
   }
 }
