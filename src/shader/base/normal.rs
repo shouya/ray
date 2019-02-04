294f755fa23f2ff344ac0ab27f79cf0e39f6009e
Shou Ya <shouya@users.noreply.github.com>
2019-02-04 10:09:19 +0800

    ready to run cargo fix

--- src/shader/base/normal.rs
+++ src/shader/base/normal.rs
@@ -5,7 +5,7 @@ use shader::{DynValue, Incidence, Shader};
 pub struct Normal;
 
 impl Shader for Normal {
-    fn render(&self, s: &Scene, i: &Incidence) -> Option<Color> {
+    fn render(&self, _s: &Scene, i: &Incidence) -> Option<Color> {
         let n = i.hit.norm;
         let f = |v| 0.5 * (v + 1.0);
         let c = Color([f(n.x()), f(n.y()), f(n.z())]);
